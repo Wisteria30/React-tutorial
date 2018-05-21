8974c739fb32ed1c13517a27d05cec5552796ec4
Wisteria30 <skymars0327@yahoo.co.jp>
2018-05-21 16:06:11 +0900

    コンポ念とか

--- src/Square.js
+++ src/Square.js
@@ -1,3 +1,10 @@
 import * as React from 'react';
 
-//export default Square;
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+export default Square;
